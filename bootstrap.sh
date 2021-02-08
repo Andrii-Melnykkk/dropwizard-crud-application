fe9a18414ee3001967beb6dfe771abd0e18d5b21
Andrii Melnyk <qwertynewbie777@gmail.com>
2021-02-08 18:45:02 +0200

    test commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 /opt/mssql-tools/bin/sqlcmd
 
 # show db
-# /opt/mssql-tools/bin/sqlcmd -S sql -U SA -P 'Pass123456!@#' -Q "SELECT name FROM sys.databases"
+# /opt/mssql-tools/bin/sqlcmd -S sql -U SA -P User12333! -Q "SELECT name FROM sys.databases"
 # /opt/mssql-tools/bin/sqlcmd -S sql -U SA -P 'Pass123456!@#' -Q "SELECT name, state_desc FROM  sys.databases"
 
 # run init sql
